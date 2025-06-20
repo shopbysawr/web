d9251e67c571e9168b0b9db6175e94b8b94349f0
bySawR <141033049+bySawR@users.noreply.github.com>
2025-06-20 22:43:20 +0200

    o

--- sidemenu.js
+++ sidemenu.js
@@ -20,21 +20,34 @@
     }
 
     // Categories data (index based on ul children, 1-based)
-    const categories = [
-        { main: 4, collapsible: [5, 6, 7, 8] },
-      { main: 9, collapsible: [10, 11, 12, 13] },
-      { main: 14, collapsible: [15, 16, 17, 18, 19] },
-      { main: 20, collapsible: [21, 22, 23, 24, 25, 26, 27, 28] },
-      { main: 39, collapsible: [40, 41, 42, 43, 44, 45, 46, 47] },
-      { main: 48, collapsible: [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66] },
-      { main: 75, collapsible: [76, 77] },
-      { main: 79, collapsible: [80, 81] },
-      { main: 82, collapsible: [83, 84, 85, 86] },
-      { main: 87, collapsible: [88, 89, 90, 91, 92, 93] },
-      { main: 94, collapsible: [95, 96, 97, 98] },
-      { main: 99, collapsible: [100, 101, 102, 103] },
-      { main: 106, collapsible: [107, 108] },
-    ];
+const categories = [
+  { main: 4, collapsible: [5, 6, 7, 8] },
+  { 
+    main: 38,
+    collapsible: [
+      39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+      51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
+      63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+      75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+      87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+      99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+      109, 110, 111, 112, 113
+    ]
+  },
+  { main: 9, collapsible: [10, 11, 12, 13] },
+  { main: 14, collapsible: [15, 16, 17, 18, 19] },
+  { main: 20, collapsible: [21, 22, 23, 24, 25, 26, 27, 28] },
+  { main: 39, collapsible: [40, 41, 42, 43, 44, 45, 46, 47] },
+  { main: 48, collapsible: [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66] },
+  { main: 75, collapsible: [76, 77] },
+  { main: 79, collapsible: [80, 81] },
+  { main: 82, collapsible: [83, 84, 85, 86] },
+  { main: 87, collapsible: [88, 89, 90, 91, 92, 93] },
+  { main: 94, collapsible: [95, 96, 97, 98] },
+  { main: 99, collapsible: [100, 101, 102, 103] },
+  { main: 106, collapsible: [107, 108] },
+];
+
 
     function collapseAll() {
       categories.forEach(category => {
